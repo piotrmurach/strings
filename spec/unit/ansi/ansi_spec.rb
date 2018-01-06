2157f4b64674abac5d52bae17dc8bbee5669f0dc
Piotr Murach <pmurach@gmail.com>
2018-01-06 16:24:38 +0100

    Add specs for ansi helper

--- spec/unit/ansi/ansi_spec.rb
+++ spec/unit/ansi/ansi_spec.rb
@@ -1,9 +1,17 @@
 # frozen_string_literal: true
 
-RSpec.describe Strings::ANSI, '.ansi?' do
+RSpec.describe Strings::ANSI, '#ansi?' do
   subject(:sanitizer) { described_class }
 
+  it "deson't report empty string" do
+    expect(Strings::ANSI.ansi?('')).to eq(false)
+  end
+
+  it "reports non-ansi string correctly" do
+    expect(Strings::ANSI.ansi?("foo")).to eq(false)
+  end
+
   it "checks if code is ansi" do
-    expect(Strings::ANSI.ansi?("\e[0;33m")).to eq(true)
+    expect(Strings::ANSI.ansi?("\e[33;44mfoo\e[0m")).to eq(true)
   end
 end
